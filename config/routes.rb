da63b87d7afc2ef402c602c7cb7213003af74966
Shlok <shlok.srivastava15@gmail.com>
2015-11-21 19:59:21 +0000

    Created Registration Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   get 'static_pages/home'
-
   get 'static_pages/contact_us'
-
+  get ':action' => 'static#:action'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
